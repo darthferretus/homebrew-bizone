4f97873dd81ff79d177e399d880a3e18bd5cb02f
Sergey Rusakov <lord@sith.space>
2020-10-23 15:55:54 +0300

    Add jsonnet@0.16.0 as jsonnet

---
+++ Formula/jsonnet.rb
@@ -0,0 +1,46 @@
+class JsonnetAT0160 < Formula
+  desc "Domain specific configuration language for defining JSON data"
+  homepage "https://jsonnet.org/"
+  url "https://github.com/google/jsonnet/archive/v0.16.0.tar.gz"
+  sha256 "fa1a4047942797b7c4ed39718a20d63d1b98725fb5cf563efbc1ecca3375426f"
+  license "Apache-2.0"
+  head "https://github.com/google/jsonnet.git"
+
+  livecheck do
+    url "https://github.com/google/jsonnet/releases/latest"
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  def install
+    ENV.cxx11
+    system "make"
+    bin.install "jsonnet"
+    bin.install "jsonnetfmt"
+  end
+
+  test do
+    (testpath/"example.jsonnet").write <<~EOS
+      {
+        person1: {
+          name: "Alice",
+          welcome: "Hello " + self.name + "!",
+        },
+        person2: self.person1 { name: "Bob" },
+      }
+    EOS
+
+    expected_output = {
+      "person1" => {
+        "name"    => "Alice",
+        "welcome" => "Hello Alice!",
+      },
+      "person2" => {
+        "name"    => "Bob",
+        "welcome" => "Hello Bob!",
+      },
+    }
+
+    output = shell_output("#{bin}/jsonnet #{testpath}/example.jsonnet")
+    assert_equal expected_output, JSON.parse(output)
+  end
+end
